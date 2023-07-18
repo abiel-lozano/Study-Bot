aa09056adedc136f70ba190be997a778444028d4
abiel-lozano <73564559+abiel-lozano@users.noreply.github.com>
2023-07-18 17:15:39 -0600

    Update ColorDetection.py
    Corrected color ranges for all 3 new models

--- ObjectIdentification/ColorDetection.py
+++ ObjectIdentification/ColorDetection.py
@@ -15,11 +15,14 @@ liverUpper = np.array([41, 255, 255], np.uint8)
 stomachLower = np.array([90, 80, 1], np.uint8)
 stomachUpper = np.array([120, 255, 255], np.uint8)
 
-brainLower = np.array([170, 255 * 0.60, 255 * 0.40], np.uint8)
-brainUpper = np.array([190, 255 * 0.40, 255], np.uint8)
+brainLower = np.array([168, 255 * 0.50, 255 * 0.40], np.uint8)
+brainUpper = np.array([168, 255, 255], np.uint8)
 
-kidneyLower = np.array([21.5, 255 * 0.60, 255 * 0.49], np.uint8)
-kidneyUpper = np.array([24, 255 * 0.57, 255 * 0.82], np.uint8)
+kidneyLower = np.array([26, 255 * 0.60, 255 * 0.49], np.uint8)
+kidneyUpper = np.array([26, 255, 255], np.uint8)
+
+heartLower = np.array([179, 255 * 0.50, 255 * 0.45], np.uint8)
+heartUpper = np.array([179, 255 * 0.97, 255 * 0.69], np.uint8)
 
 while True:
     # Reading the video from the webcam in image frames
@@ -35,6 +38,8 @@ while True:
     liverMask = cv2.inRange(hsvFrame, liverLower, liverUpper)
     stomachMask = cv2.inRange(hsvFrame, stomachLower, stomachUpper)
     brainMask = cv2.inRange(hsvFrame, brainLower, brainUpper)
+    kidneyMask = cv2.inRange(hsvFrame, kidneyLower, kidneyUpper)
+    heartMask = cv2.inRange(hsvFrame, heartLower, heartUpper)
 
     # Create a 5x5 square-shaped filter called kernel
     # The filter is filled with ones and will be used for morphological transformations such as dilation for better detection
@@ -57,6 +62,14 @@ while True:
     # For brain
     brainMask = cv2.dilate(brainMask, kernel)
     resBrain = cv2.bitwise_and(imageFrame, imageFrame, mask=brainMask)
+
+    # For heart
+    heartMask = cv2.dilate(heartMask, np.ones((12, 12), "uint8"))
+    resHeart = cv2.bitwise_and(imageFrame, imageFrame, mask=heartMask)
+    
+    # For kidney use a more aggressive kernel for dilation
+    kidneyMask = cv2.dilate(kidneyMask, np.ones((12, 12), "uint8"))
+    resKidney = cv2.bitwise_and(imageFrame, imageFrame, mask=kidneyMask)
 
     # Create a contour around the zone that matches the color range
     contours, hierarchy = cv2.findContours(colonMask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE) 
@@ -81,8 +94,6 @@ while True:
 
     contours, hierarchy = cv2.findContours(stomachMask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     for pic, contour in enumerate(contours):
-        print(contours)
-        quit()
         area = cv2.contourArea(contour)
         if area > 1400:
             x, y, w, h = cv2.boundingRect(contour)
@@ -92,13 +103,30 @@ while True:
 
     contours, hierarchy = cv2.findContours(brainMask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     for pic, contour in enumerate(contours):
-        print("Brain detected")
         area = cv2.contourArea(contour)
-        if area > 5:
+        if area > 500:
             x, y, w, h = cv2.boundingRect(contour)
             imageFrame = cv2.rectangle(imageFrame, (x, y), (x + w, y + h), (204, 0, 255), 2)
             # cv2.putText(imageFrame, "BRAIN", (x, y), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (0, 0, 255))
             # print("Brain detected")
+
+    contours, hierarchy = cv2.findContours(kidneyMask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    for pic, contour in enumerate(contours):
+        area = cv2.contourArea(contour)
+        if area > 50:
+            x, y, w, h = cv2.boundingRect(contour)
+            imageFrame = cv2.rectangle(imageFrame, (x, y), (x + w, y + h), (0, 255, 255), 2)
+            # cv2.putText(imageFrame, "KIDNEY", (x, y), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (0, 255, 255))
+            # print("Kidney detected")
+
+    contours, hierarchy = cv2.findContours(heartMask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    for pic, contour in enumerate(contours):
+        area = cv2.contourArea(contour)
+        if area > 200:
+            x, y, w, h = cv2.boundingRect(contour)
+            imageFrame = cv2.rectangle(imageFrame, (x, y), (x + w, y + h), (0, 0, 255), 2)
+            # cv2.putText(imageFrame, "HEART", (x, y), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (255, 0, 0))
+            # print("Heart detected")
 
     # Display camera feed
     cv2.imshow("Color-Based Object Detection", imageFrame)
