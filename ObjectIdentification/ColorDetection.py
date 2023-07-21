ba9041a9f69a09bfc2e346b40e5db7996b0db16f
abiel-lozano <73564559+abiel-lozano@users.noreply.github.com>
2023-07-20 23:05:06 -0600

    Read summary for changes
    Refactored StudyBot module, added source for new organs, more tests using Tkinter.

--- ObjectIdentification/ColorDetection.py
+++ ObjectIdentification/ColorDetection.py
@@ -8,21 +8,18 @@ import cv2
 cam = cv2.VideoCapture(0)
 
 # Color ranges
-colonLower = np.array([9, 255 * 0.55, 255 * 0.35], np.uint8)
-colonUpper = np.array([9, 255, 255], np.uint8)
-liverLower = np.array([38, 225 * 0.22, 255 * 0.38], np.uint8)
-liverUpper = np.array([41, 255, 255], np.uint8)
-stomachLower = np.array([90, 80, 1], np.uint8)
-stomachUpper = np.array([120, 255, 255], np.uint8)
-
-brainLower = np.array([168, 255 * 0.50, 255 * 0.40], np.uint8)
-brainUpper = np.array([168, 255, 255], np.uint8)
-
-kidneyLower = np.array([26, 255 * 0.60, 255 * 0.49], np.uint8)
-kidneyUpper = np.array([26, 255, 255], np.uint8)
-
-heartLower = np.array([179, 255 * 0.50, 255 * 0.45], np.uint8)
-heartUpper = np.array([179, 255 * 0.97, 255 * 0.69], np.uint8)
+stomachLower = np.array([90, 	80, 		1			], np.uint8)
+stomachUpper = np.array([120, 	255, 		255			], np.uint8)
+colonLower = np.array(	[9, 	255 * 0.55, 255 * 0.35	], np.uint8)
+colonUpper = np.array(	[28, 	255, 		255			], np.uint8)
+liverLower = np.array(	[38, 	225 * 0.22, 255 * 0.38	], np.uint8)
+liverUpper = np.array(	[41, 	255, 		255			], np.uint8)
+brainLower = np.array(	[168, 	255 * 0.50, 255 * 0.40	], np.uint8)
+brainUpper = np.array(	[168, 	255, 		255			], np.uint8)
+kidneyLower = np.array(	[26, 	255 * 0.60, 255 * 0.49	], np.uint8)
+kidneyUpper = np.array(	[26, 	255, 		255			], np.uint8)
+heartLower = np.array(	[179, 	255 * 0.50, 255 * 0.45	], np.uint8)
+heartUpper = np.array(	[179, 	255 * 0.97, 255 * 0.69	], np.uint8)
 
 while True:
     # Reading the video from the webcam in image frames
--- Tests/TkinterTesting.py
+++ Tests/TkinterTesting.py
@@ -1,7 +1,3 @@
-# Colors:
-# > #09B898 = teal
-# > #9ae6d8 = light teal
-
 import tkinter as tk
 from tkinter import messagebox
 
@@ -13,51 +9,51 @@ def check_selection():
 # Create the main window
 window = tk.Tk()
 window.title("Study-Bot")
-window.geometry("300x315")  # Set the window size
+window.geometry("450x470")
 
 # Set the background color
 window.configure(bg="#09B898")
 
 # Create the title label
-titleLabel = tk.Label(window, text="Study-Bot", font=("Nunito", 16, "bold"), bg="#9ae6d8")
-titleLabel.pack(pady=10)
+titleLabel = tk.Label(window, text="Study-Bot", font=("Nunito", 24, "bold"), bg="#9ae6d8")
+titleLabel.pack(pady=15)
 
 # Create the language dropdown
-languageLabel = tk.Label(window, text="Select Language:", bg="#9ae6d8")
-languageLabel.pack(pady=10)  # Add vertical spacing
+languageLabel = tk.Label(window, text="Select Language:", bg="#9ae6d8", font=("Nunito", 12))
+languageLabel.pack(pady=15)
 languageVar = tk.StringVar(window)
 languageDropdown = tk.OptionMenu(window, languageVar, "English", "Spanish")
-languageDropdown.config(width=10)  # Increase the width of the dropdown
+languageDropdown.config(width=15)
 languageDropdown.pack()
 
 # Create the topic dropdown
-topicLabel = tk.Label(window, text="Select Topic:", bg="#9ae6d8")
-topicLabel.pack(pady=10)  # Add vertical spacing
+topicLabel = tk.Label(window, text="Select Topic:", bg="#9ae6d8", font=("Nunito", 12))
+topicLabel.pack(pady=15)
 topicVar = tk.StringVar(window)
 topicDropdown = tk.OptionMenu(window, topicVar, "Chemistry", "Physics")
-topicDropdown.config(width=10)  # Increase the width of the dropdown
+topicDropdown.config(width=15)
 topicDropdown.pack()
 
 # Create the "Check" button
-checkButton = tk.Button(window, text="Check", command=check_selection, bg="#9ae6d8")
-checkButton.pack(pady=10)  # Add vertical spacing
+checkButton = tk.Button(window, text="Check", command=check_selection, bg="#9ae6d8", font=("Nunito", 12))
+checkButton.pack(pady=15)
 
 # Create the selection text label
 selectionText = tk.StringVar()
-selectionLabel = tk.Label(window, textvariable=selectionText, bg="#9ae6d8")
+selectionLabel = tk.Label(window, textvariable=selectionText, bg="#9ae6d8", font=("Nunito", 12))
 selectionLabel.pack()
 
 # Create the buttons frame
 buttonsFrame = tk.Frame(window, bg="#09B898")
-buttonsFrame.pack(pady=10)
+buttonsFrame.pack(pady=15)
 
 # Create the "Ask another question" button
-askButton = tk.Button(buttonsFrame, text="Ask another question", bg="#9ae6d8")
-askButton.pack(side="left", padx=5)
+askButton = tk.Button(buttonsFrame, text="Ask another question", bg="#9ae6d8", font=("Nunito", 12))
+askButton.pack(side="left", padx=10)
 
 # Create the "Exit" button
-exitButton = tk.Button(buttonsFrame, text="Exit", bg="#9ae6d8")
-exitButton.pack(side="left", padx=5)
+exitButton = tk.Button(buttonsFrame, text="Exit", bg="#9ae6d8", font=("Nunito", 12))
+exitButton.pack(side="left", padx=10)
 
 # Run the main event loop
 window.mainloop()
