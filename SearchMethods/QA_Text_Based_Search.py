d0777e9e0d20adc405d11760b5467df0572abf5e
abiel-lozano <73564559+abiel-lozano@users.noreply.github.com>
2023-09-30 11:59:05 -0600

    AI Model change, corrected color ranges.
    Last commit before possibly breakinbg things.

--- SearchMethods/QA_Text_Based_Search.py
+++ SearchMethods/QA_Text_Based_Search.py
@@ -8,7 +8,7 @@ print('Study-Bot: Question Answering (Text-Based Search)', end='\n\n')
 
 # OpenAI API Configuration
 EMBEDDING_MODEL = 'text-embedding-ada-002'
-GPT_MODEL = 'gpt-3.5-turbo'
+GPT_MODEL = 'gpt-3.5-turbo-16k'
 
 # Set up OpenAI API credentials
 openai.api_key = '' # API KEY HERE
@@ -27,7 +27,19 @@ source = ''
 if topic == '1':
     print('Topic: Neurulation')
     source = """
-    
+    Neurulation is a critical process during embryonic development that leads to the formation of the neural tube, which eventually gives rise to the central nervous system (CNS). Here is a 5-stage summary of the neurulation process:
+
+Initiation: Neurulation begins during the third week of embryonic development. It is initiated when the notochord, a rod-like structure that runs along the embryonic midline, induces the overlying ectoderm to differentiate into neuroectoderm. This neuroectoderm is the precursor tissue for the nervous system.
+
+Formation of the Neural Plate: The induced neuroectoderm thickens and forms a flat, plate-like structure called the neural plate. This plate is located along the dorsal (back) surface of the embryo and extends from the cranial (head) end to the caudal (tail) end.
+
+Folding of the Neural Plate: As development progresses, the neural plate undergoes a complex series of folding events. The edges of the plate elevate and move towards each other, forming two parallel ridges called the neural folds. This folding process begins in the cranial region and proceeds towards the caudal region.
+
+Formation of the Neural Tube: The neural folds continue to elevate and eventually fuse together along the midline of the embryo. This fusion begins in the cervical (neck) region and proceeds both cranially and caudally. Once the neural folds have completely fused, they form a hollow, tube-like structure called the neural tube. This tube will give rise to the brain and spinal cord.
+
+Differentiation of Neural Crest Cells: During the process of neurulation, some cells at the neural crest are not incorporated into the neural tube but instead migrate to other areas of the embryo. These neural crest cells will differentiate into various cell types, including peripheral neurons, glial cells, and cells of the peripheral nervous system, as well as other non-neural cell types like cartilage, bone, and pigment cells.
+
+The completion of neurulation is a crucial milestone in embryonic development, as it sets the stage for the formation of the entire central nervous system and other related structures. Any disruptions or abnormalities in this process can lead to serious congenital defects, such as neural tube defects like spina bifida and anencephaly.
     """
 elif topic == '2':
     print('Topic: Pharyngeal Apparatus')
